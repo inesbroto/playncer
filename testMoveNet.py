3fa27574b75bc567ba4939ec953b6e725cd6cd5e
inesbroto <49390205+inesbroto@users.noreply.github.com>
2024-11-05 20:03:47 +0100

    Update testMoveNet.py
    afegir grav center. Els prints a la pantalla no son correctes

--- testMoveNet.py
+++ testMoveNet.py
@@ -32,6 +32,16 @@ def compute_bbox(keypoints, threshold=0):
     except Exception as e:
         print(f"Exception in compute_bbox: {e}")
 
+def compute_gravity_center(keypoints, threshold=0):
+    try:
+        valid_keypoints = tf.gather(keypoints[0][0], indices=tf.where(keypoints[0][0][:,2]>threshold).numpy(), axis=0)
+        valid_keypoints = tf.reshape(valid_keypoints, [valid_keypoints.shape[0],valid_keypoints.shape[2]])
+        x_coord =tf.reduce_mean(valid_keypoints[:,0]).numpy()
+        y_coord =tf.reduce_mean(valid_keypoints[:,1]).numpy()
+
+        return (x_coord, y_coord)
+    except Exception as e:
+        print(f"Exception in compute_gravity_center: {e}")
 
 
 
@@ -45,9 +55,19 @@ def load_moveNet_model():
         print(f"Exception in loading the model: {e}")
 
 
-def draw_keypoints(keypoints, img, radius = 2, color = (0,180,0), thickness=2):
+
+def draw_point(img,x_coord, y_coord, radius = 2, color = (0,180,0), thickness=2):
     try:
         y, x, _ = img.shape
+        img = cv2.circle(img, (int(x_coord*x), int(y_coord*y)), radius, color, thickness)
+
+        return img
+    except Exception as e:
+        print(f"Exception in draw_point: {e}")
+
+def draw_keypoints(img, keypoints, radius = 2, color = (0,180,0), thickness=2, threshold=0):
+    try:
+        #y, x, _ = img.shape
         # iterate through keypoints
         for idx, k in enumerate(keypoints[0,0,:,:]):
             # Converts to numpy array
@@ -55,11 +75,12 @@ def draw_keypoints(keypoints, img, radius = 2, color = (0,180,0), thickness=2):
             # Checks confidence for keypoint
             if k[2] > threshold:
                 # The first two channels of the last dimension represents the yx coordinates (normalized to image frame, i.e. range in [0.0, 1.0]) of the 17 keypoints
-                yc = int(k[0] * y)
-                xc = int(k[1] * x)
+                #yc = int(k[0] * y)
+                #xc = int(k[1] * x)
 
+                img = draw_point(img=img, x_coord=k[0],y_coord= k[1], radius=radius, color=color, thickness=thickness)
                 # Draws a circle on the image for each keypoint
-                img = cv2.circle(img, (xc, yc), radius, color, thickness)
+                #img = cv2.circle(img, (xc, yc), radius, color, thickness)
 
 
         return img
@@ -67,13 +88,13 @@ def draw_keypoints(keypoints, img, radius = 2, color = (0,180,0), thickness=2):
         print(f"Exception in draw_keypoints: {e}")
 
 
-def draw_bbox(x_coord, y_coord, height, length, img, color = (180,0,0), thickness=2):
+def draw_bbox(img, x_coord, y_coord, height, length, color = (180,0,0), thickness=2):
     try:
         y, x, _ = img.shape
         low_left = (int(x_coord*x), int(y_coord*y))
         low_right = (int((x_coord+length)*x), int(y_coord*y))
         up_left = (int(x_coord*x), int((y_coord+height)*y))
-        up_right = ((int(x_coord+length)*x), int((y_coord+height)*y))
+        up_right = (int((x_coord+length)*x), int((y_coord+height)*y))
 
         cv2.line(img, low_left, low_right, color, thickness) 
         cv2.line(img, low_left, up_left, color, thickness) 
@@ -98,7 +119,7 @@ if __name__ == "__main__":
         OSC_client = udp_client.SimpleUDPClient(args.ip, args.port)
         movenet = load_moveNet_model()
         # Threshold for 
-        threshold = .5
+        threshold = .03
 
         # Loads video source (0 is for main webcam)
         video_source = 0
@@ -145,18 +166,22 @@ if __name__ == "__main__":
             keypoints = outputs['output_0']
 
             
-            x_bbox, y_bbox, height_bbox, length_bbox = compute_bbox(keypoints, threshold=threshold)
+            x_bbox, y_bbox, length_bbox,height_bbox = compute_bbox(keypoints, threshold=threshold)
             bbox_area = height_bbox*length_bbox
+            grav_center = compute_gravity_center(keypoints, threshold=threshold)
 
 
-            img = draw_keypoints(keypoints, img)
-            img = draw_bbox(x_bbox, y_bbox, height_bbox, length_bbox, img)
+            img = draw_keypoints(img = img, keypoints=keypoints)
+            img = draw_bbox(img=img,x_coord=x_bbox, y_coord=y_bbox, height=height_bbox, length=length_bbox)
+            img = draw_point(img=img, x_coord=grav_center[0], y_coord=grav_center[1])
 
             freq = keypoints[0][0][0][0].numpy()*200 +200
             
-            print(freq, "    |     ",  x_bbox, y_bbox, height_bbox, length_bbox)
+            print(freq, "  |  ",  x_bbox, y_bbox, height_bbox, length_bbox, "  |  ", grav_center)
+            #print(keypoints)
             #OSC_client.send_message("/freq", freq)
 
+            #time.sleep(10)
             # Shows image
             cv2.imshow('Movenet', img)
             # Waits for the next frame, checks if q was pressed to quit
